01595265580ca7a524391f222d4363ed3a9fa587
Alban Mouton <alban.mouton@gmail.com>
2021-01-18 09:37:54 +0100

    fix: typo in superadmin middleware

--- public/middleware/superadmin-required.js
+++ public/middleware/superadmin-required.js
@@ -4,7 +4,7 @@ export default async function ({ store, error }) {
       message: 'Vous devez être authentifié pour accéder à cette page.',
       statusCode: 401,
     })
-  } else if (!store.state.session.user.adminAdmin) {
+  } else if (!store.state.session.user.adminMode) {
     error({
       message: 'Vous n\'avez pas la permission d\'accéder à cette page, il faut avoir activé le mode administration.',
       statusCode: 403,
