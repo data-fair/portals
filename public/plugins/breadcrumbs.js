c2d43cafc6e3ef3a2e752e9f72b9dd6d86cbcf6c
Alban Mouton <alban.mouton@gmail.com>
2021-03-10 18:51:12 +0100

    fix: better management of redirect

--- public/plugins/breadcrumbs.js
+++ public/plugins/breadcrumbs.js
@@ -2,7 +2,7 @@ export default async ({ route, redirect }) => {
   // dynamic navigation from the iframe parent
   window.addEventListener('message', (e) => {
     if (e.data && typeof e.data === 'object' && 'to' in e.data && e.data.to !== route.query.to) {
-      window.$nuxt.$router.replace(route.query.to)
+      window.$nuxt.$router.replace(e.data.to)
     }
   })
 }
