7ead46f6696ca4d0c1f2b5db0ad4180702578e95
Alban Mouton <alban.mouton@gmail.com>
2021-03-15 14:49:20 +0100

    design: uniform colors with other services

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,10 +80,14 @@ module.exports = {
     theme: {
       themes: {
         light: {
-          primary: '#1E88E5', // colors.blue.darken1
-          // primary: colors.blue.lighten1, // code near our logo 'dark' blue
-          accent: '#F57C00', // colors.orange.darken2
-          warning: '#F57C00', // colors.orange.darken2
+          primary: '#1E88E5', // blue.darken1
+          secondary: '#42A5F5', // blue.lighten1,
+          accent: '#FF9800', // orange.base
+          error: 'FF5252', // red.accent2
+          info: '#2196F3', // blue.base
+          success: '#4CAF50', // green.base
+          warning: '#E91E63', // pink.base
+          admin: '#E53935', // red.darken1
         },
         dark: {
           primary: '#42A5F5', // colors.blue.lighten1,
