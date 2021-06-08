ffb8d88076a29944722d30a45a6dd785e0a009f3
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2021-06-08 09:15:15 +0200

    feat: WIP on new responsive layout element

--- public/components/pages/blank.vue
+++ public/components/pages/blank.vue
@@ -1,11 +1,6 @@
 <template>
   <v-container v-if="config">
     <template v-for="(element, i) in (config.elements || []).filter(e => e)">
-      <k-element
-        v-if="!element.type.includes('Columns')"
-        :key="i"
-        :value="element"
-      />
       <v-row v-if="element.type === 'twoColumns'" :key="i">
         <v-col :cols="12" :md="element.layout === 'left' ? 8 : (element.layout === 'right' ? 4 : 6)">
           <k-element
@@ -22,6 +17,31 @@
           />
         </v-col>
       </v-row>
+      <v-row v-else-if="element.type === 'responsiveFlow'" :key="i">
+        <v-col
+          v-for="(lElement, li) in element.items"
+          :key="`${i}l${li}`"
+          :cols="12"
+          :sm="6"
+          :md="4"
+        >
+          <k-element
+            :value="lElement"
+          />
+        </v-col>
+        <v-col :cols="12" :md="element.layout === 'left' ? 4 : (element.layout === 'right' ? 8 : 6)">
+          <k-element
+            v-for="(rElement, ri) in element.rightColumn"
+            :key="`${i}r${ri}`"
+            :value="rElement"
+          />
+        </v-col>
+      </v-row>
+      <k-element
+        v-else
+        :key="i"
+        :value="element"
+      />
     </template>
   </v-container>
 </template>
