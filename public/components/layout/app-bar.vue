e995000ccdfba6394e952a084026836621dbd26e
Alban Mouton <alban.mouton@gmail.com>
2022-01-19 15:27:26 +0100

    design: bold navigation menu

--- public/components/layout/app-bar.vue
+++ public/components/layout/app-bar.vue
@@ -63,6 +63,7 @@
               text
               v-bind="attrs"
               :height="64"
+              class="font-weight-bold"
               :class="{'white--text': themeColorDark}"
               v-on="on"
             >
