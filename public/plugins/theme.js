5da0c72063cdcd30c763927f8825e03b34f8f27b
Alban Mouton <alban.mouton@gmail.com>
2022-04-20 10:40:26 +0200

    chore: add debug log

--- public/plugins/theme.js
+++ public/plugins/theme.js
@@ -1,7 +1,12 @@
+const debug = require('debug')('portals:theme')
+debug.log = console.log.bind(console)
 
 export default async ({ store, $vuetify }) => {
   if (store.state.config) {
     $vuetify.theme.themes.light.primary = store.state.config.themeColor
+    debug('apply color from config in store', store.state.config.themeColor)
+  } else {
+    debug('keep color from original vuetify config', $vuetify.theme.themes.light.primary)
   }
   /* TODO: add store.state.config.themeColorDark ?
   if (app.$cookies.get('theme_dark') !== undefined) {
