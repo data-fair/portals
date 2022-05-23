06a466d9396a04d969bfc9e630c960e994a3b056
Alban Mouton <alban.mouton@gmail.com>
2022-05-23 21:34:49 +0200

    feat: use theme_dark cookie in manager pages

--- public/plugins/theme.js
+++ public/plugins/theme.js
@@ -1,15 +1,14 @@
 const debug = require('debug')('portals:theme')
 debug.log = console.log.bind(console)
 
-export default async ({ store, $vuetify }) => {
+export default async ({ app, store, $vuetify, route }) => {
   if (store.state.inPortal && store.state.config) {
     $vuetify.theme.themes.light.primary = store.state.config.themeColor
     debug('apply color from config in store', store.state.config.themeColor)
   } else {
     debug('keep color from original vuetify config', $vuetify.theme.themes.light.primary)
   }
-  /* TODO: add store.state.config.themeColorDark ?
-  if (app.$cookies.get('theme_dark') !== undefined) {
+  if (route.path.startsWith('/manager/') && app.$cookies.get('theme_dark') !== undefined) {
     $vuetify.theme.dark = app.$cookies.get('theme_dark')
-  } */
+  }
 }
