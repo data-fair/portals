d527ca6e0891df5a0d2f32d966280d13efb91a48
Alban Mouton <alban.mouton@gmail.com>
2022-07-21 14:19:39 +0200

    chore: remove temporary /table2 preview url

--- config/development.js
+++ config/development.js
@@ -2,6 +2,6 @@ module.exports = {
   notifyUrl: 'http://localhost:6200/notify',
   privateNotifyUrl: 'http://localhost:8088',
   notifyWSUrl: 'ws://localhost:8088',
-  proxyNuxt: true,
-  tablePreviewPath: '/table2'
+  proxyNuxt: true
+  // tablePreviewPath: '/table2'
 }
