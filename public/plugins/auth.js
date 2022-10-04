316899b4d8f0282194a0968ffe4d66ae10f504a5
Alban Mouton <alban.mouton@gmail.com>
2022-10-04 10:09:31 +0200

    fix: broken forced auth redirect

--- public/plugins/auth.js
+++ public/plugins/auth.js
@@ -1,6 +1,10 @@
 export default ({ store, env, app }) => {
   if (!store.state.config) return
   if (store.state.config.authentication === 'required' && (!store.state.session || !store.state.session.user)) {
-    return store.dispatch('session/login', window.location.href)
+    window.location.href = store.getters['session/loginUrl'](
+      window.location.href,
+      false,
+      { org: store.state.config.owner.type === 'organization' ? store.state.config.owner.id : '', primary: store.state.config.themeColor }
+    )
   }
 }
