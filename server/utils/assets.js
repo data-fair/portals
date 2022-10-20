8b5c444b99e2f9df565470b1542ff93941ba1e97
Alban Mouton <alban.mouton@gmail.com>
2022-10-20 12:06:58 +0200

    chore: add some debug info to assets router

--- server/utils/assets.js
+++ server/utils/assets.js
@@ -5,6 +5,7 @@ const sharp = require('sharp')
 const hasha = require('hasha')
 const multer = require('multer')
 const mime = require('mime-types')
+const debug = require('debug')('assets')
 
 const assets = {
   logo: {
@@ -35,6 +36,7 @@ exports.prepareFitHashedAsset = async (dir, asset) => {
   const originalBuffer = await fs.readFile(path.join(dir, asset))
   let buffer
   try {
+    debug('use sharp to prepare resized image', asset, assets[asset])
     buffer = await sharp(originalBuffer)
       .resize(assets[asset].size.width, assets[asset].size.height, { fit: 'inside', withoutEnlargement: true })
       .png({ adaptiveFiltering: true, compressionLevel: 9, palette: true })
@@ -45,15 +47,18 @@ exports.prepareFitHashedAsset = async (dir, asset) => {
   const files = await fs.readdir(dir)
   for (const file of files) {
     if (file.startsWith(asset + '-')) {
+      debug('remove previous version of the asset', file)
       await fs.remove(path.join(dir, file))
     }
   }
   let hash
   if (buffer && buffer.length <= originalBuffer.length) {
     hash = 'fit-' + (await hasha.async(buffer)).slice(0, 10) + '.png'
+    debug('new version of asset is smaller, use it', `${asset}-${hash}`)
     await fs.writeFile(path.join(dir, `${asset}-${hash}`), buffer)
   } else {
     hash = (await hasha.async(originalBuffer)).slice(0, 10)
+    debug('new version of asset is not smaller, use original', `${asset}-${hash}`)
     await fs.writeFile(path.join(dir, `${asset}-${hash}`), originalBuffer)
   }
   return hash
@@ -63,13 +68,20 @@ exports.fillConfigAssets = async (dir, conf, force) => {
   if (!await fs.pathExists(dir)) return
   const files = await fs.readdir(dir)
   for (const asset in conf.assets || {}) {
-    if (!files.includes(asset)) delete conf.assets[asset]
-    else {
+    if (!files.includes(asset)) {
+      debug('file not found for asset, remove reference', asset)
+      delete conf.assets[asset]
+    } else {
       const hashedFile = files.find(f => f.startsWith(asset + '-'))
       // this condition should not be necessary, here only to complete missing hashed images from older portals
       // TODO: if the resized file is not smaller than original do not store hash in config ?
-      if (hashedFile && !force) conf.assets[asset].hash = hashedFile.replace(asset + '-', '')
-      else conf.assets[asset].hash = await exports.prepareFitHashedAsset(dir, asset)
+      if (hashedFile && !force) {
+        debug('use hashed version of file', hashedFile)
+        conf.assets[asset].hash = hashedFile.replace(asset + '-', '')
+      } else {
+        debug('process new hash of asset', asset)
+        conf.assets[asset].hash = await exports.prepareFitHashedAsset(dir, asset)
+      }
     }
   }
 }
