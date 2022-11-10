bb1b77ee1247e3727d659a38b0d8113f4e806b48
Alban Mouton <alban.mouton@gmail.com>
2022-11-10 09:44:48 +0100

    feat: better kpi for accessibility

--- public/components/kpi.vue
+++ public/components/kpi.vue
@@ -4,90 +4,34 @@
     justify="center"
   >
     <v-col
+      v-for="metric,i of metrics"
+      :key="i"
       sm="4"
       cols="12"
     >
       <v-card v-bind="cardProps">
         <v-card-title class="text-center pb-2">
           <v-col class="pa-0">
-            <h3
+            <span
               class="headline grey--text text--darken-3 font-weight-bold"
-              style="height:40px"
+              style="height:40px;display:block;"
             >
-              <span v-if="stats">{{ stats.datasets.count }}</span>
+              <template v-if="stats">
+                {{ metric.value }}
+              </template>
               <v-skeleton-loader
                 v-else
                 max-width="80"
                 type="button"
                 class="mx-auto"
               />
-            </h3>
-            <h3
+            </span>
+            <span
               class="title grey--text text--darken-3 font-weight-bold"
-              style="height:40px;text-transform:uppercase;"
+              style="height:40px;text-transform:uppercase;display:block;"
             >
-              Jeux de données
-            </h3>
-          </v-col>
-        </v-card-title>
-      </v-card>
-    </v-col>
-    <v-col
-      sm="4"
-      cols="12"
-    >
-      <v-card v-bind="cardProps">
-        <v-card-title class="text-center pb-2">
-          <v-col class="pa-0">
-            <h3
-              class="headline grey--text text--darken-3 font-weight-bold"
-              style="height:40px"
-            >
-              <span v-if="stats">{{ stats.datasets.numlines.toLocaleString() }}</span>
-              <v-skeleton-loader
-                v-else
-                max-width="80"
-                type="button"
-                class="mx-auto"
-              />
-            </h3>
-
-            <h3
-              class="title grey--text text--darken-3 font-weight-bold"
-              style="height:40px;text-transform:uppercase;"
-            >
-              Enregistrements
-            </h3>
-          </v-col>
-        </v-card-title>
-      </v-card>
-    </v-col>
-    <v-col
-      v-if="!config.applicationsPage || config.applicationsPage.type !== 'none'"
-      sm="4"
-      cols="12"
-    >
-      <v-card v-bind="cardProps">
-        <v-card-title class="text-center pb-2">
-          <v-col class="pa-0">
-            <h3
-              class="headline grey--text text--darken-3 font-weight-bold"
-              style="height:40px"
-            >
-              <span v-if="stats">{{ stats.applications.count }}</span>
-              <v-skeleton-loader
-                v-else
-                max-width="80"
-                type="button"
-                class="mx-auto"
-              />
-            </h3>
-            <h3
-              class="title grey--text text--darken-3 font-weight-bold"
-              style="height:40px;text-transform:uppercase;"
-            >
-              Visualisations
-            </h3>
+              {{ metric.title }}
+            </span>
           </v-col>
         </v-card-title>
       </v-card>
@@ -110,6 +54,16 @@ export default {
         class: this.kpiOptions.includes('outlined') ? 'also-outlined' : '',
         style: `background-color:${this.kpiBackgroundColor}`
       }
+    },
+    metrics () {
+      const metrics = [
+        { value: this.stats && this.stats.datasets.count, title: 'Jeux de données' },
+        { value: this.stats && this.stats.datasets.numlines, title: 'Enregistrements' }
+      ]
+      if (!this.config.applicationsPage || this.config.applicationsPage.type !== 'none') {
+        metrics.push({ value: this.stats && this.stats.applications.count, title: 'Visualisations' })
+      }
+      return metrics
     }
   }
 }
