c95c4cfa1e7b8f87b96cfdecb57075dabb925c70
Alban Mouton <alban.mouton@gmail.com>
2022-11-29 12:33:35 +0100

    feat: work on metadata attachments

--- public/components/dataset/dataset-attachments.vue
+++ public/components/dataset/dataset-attachments.vue
@@ -17,7 +17,7 @@
         dense
         flat
       >
-        <v-toolbar-title>{{ dataset.title }}</v-toolbar-title>
+        <v-toolbar-title>Pièces jointes</v-toolbar-title>
         <v-spacer />
         <v-btn
           icon
@@ -29,29 +29,20 @@
       <v-card-text class="pa-0">
         <v-list class="mb-0">
           <v-list-item
-            v-for="attachment in dataset.attachments"
-            :key="attachment.name"
+            v-for="(attachment, i) in dataset.attachments"
+            :key="i"
           >
-            <v-list-item-avatar
-              size="80"
-              class="my-0"
-            >
-              <v-row>
-                <v-col>
-                  <span>{{ attachment.name.split('.').pop().toUpperCase() }}</span>
-                  <br>
-                  <small>{{ attachment.size | bytes }}</small>
-                  <!-- <v-icon :class="[item.iconClass]">{{ item.icon }}</v-icon> -->
-                </v-col>
-              </v-row>
-            </v-list-item-avatar>
             <v-list-item-content>
-              <v-list-item-title>{{ attachment.title || attachment.name }} - <small>Mis à jour le {{ attachment.updatedAt | date('L') }}</small></v-list-item-title>
-              <v-list-item-sub-title>{{ attachment.description }}</v-list-item-sub-title>
+              <v-list-item-title>{{ attachment.title }}</v-list-item-title>
+              <v-list-item-subtitle v-if="attachment.type === 'file'">
+                <span class="text-caption">{{ attachment.name }} - {{ attachment.size | bytes }} - mis à jour le {{ attachment.updatedAt | date('L') }}</span>
+              </v-list-item-subtitle>
+              <v-list-item-subtitle v-html="attachment.description" />
             </v-list-item-content>
             <v-list-item-action>
               <v-btn
-                :href="baseUrl+'/'+attachment.name"
+                :href="attachment.url"
+                download
                 icon
                 ripple
               >
@@ -72,11 +63,6 @@ export default {
   props: ['dataset', 'color'],
   data: () => ({
     dialog: null
-  }),
-  computed: {
-    baseUrl () {
-      return `${this.$store.getters.dataFairUrl}/api/v1/datasets/${this.dataset.id}/metadata-attachments`
-    }
-  }
+  })
 }
 </script>
