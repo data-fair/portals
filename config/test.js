229de8de5018cebdc3f522e673f0bea954cffd72
Alban Mouton <alban.mouton@gmail.com>
2023-03-24 08:51:10 +0100

    chore: make test ports different from dev to prevent conflict

--- config/test.js
+++ config/test.js
@@ -1,6 +1,11 @@
 module.exports = {
+  publicUrl: 'http://localhost:6300',
+  port: 6300,
   dataDir: './data/test',
   directoryUrl: 'http://localhost:8080',
   notifyUrl: 'http://localhost:8088',
-  dataFairUrl: 'http://mock-data-fair.com'
+  dataFairUrl: 'http://mock-data-fair.com',
+  prometheus: {
+    port: 9091
+  }
 }
