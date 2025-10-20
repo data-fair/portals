d595325267b17a4b53a51c5726d45e3253a840a6
Alban Mouton <alban.mouton@gmail.com>
2025-10-20 09:55:41 +0200

    feat: add some formatting to markdown content

--- shared/markdown/index.ts
+++ shared/markdown/index.ts
@@ -1,23 +1,92 @@
 import sanitizeHtml from 'sanitize-html'
 import type { IOptions as SanitizeOptions } from 'sanitize-html'
-import { marked, type MarkedOptions } from 'marked'
+import { marked, type Tokens } from 'marked'
 
 const sanitizeOpts: SanitizeOptions = {
   allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
   allowedAttributes: {
     ...sanitizeHtml.defaults.allowedAttributes,
+    '*': ['class'],
     img: ['title', 'alt', 'src', 'srcset', 'height', 'width', 'sizes', 'loading']
   }
 }
 
 const sanitize = (html: string) => sanitizeHtml(html, sanitizeOpts)
 
-const markedOpts: MarkedOptions = {
-  // TODO: provide a renderer to apply vuetify classes
-  // OR maybe use SASS extend so that we can adjust things more easily
-  // https://sass-lang.com/documentation/at-rules/extend/
+const headingClasses: Record<number, string> = {
+  1: 'text-h2 text-primary mt-12 mb-8',
+  2: 'text-h4 mt-10 mb-6',
+  3: 'text-h5 mt-8 mb-4',
+  4: 'text-h6 mt-6 mb-4',
+  5: 'text-subtitle-1 mt-6 mb-4',
+  6: 'text-subtitle-2 mt-6 mb-4'
 }
 
-const parse = (markdown: string) => marked.parse(markdown, markedOpts) as string
+marked.use({
+  // overwrite some of the renderer function to apply vuetify classes
+  // https://github.com/markedjs/marked/blob/master/src/Renderer.ts
+  renderer: {
+    heading ({ tokens, depth }: Tokens.Heading) {
+      return `<h${depth + 1} class="${headingClasses[depth]}">${this.parser.parseInline(tokens)}</h${depth + 1}>\n`
+    },
+    hr () {
+      return '<hr class="v-divider v-theme--light" aria-orientation="horizontal" role="separator">'
+    },
+    paragraph ({ tokens }: Tokens.Paragraph) {
+      return `<p class="mb-4">${this.parser.parseInline(tokens)}</p>\n`
+    },
+    table (token: Tokens.Table) {
+      let header = ''
+
+      // header
+      let cell = ''
+      for (let j = 0; j < token.header.length; j++) {
+        cell += this.tablecell(token.header[j])
+      }
+      header += this.tablerow({ text: cell })
+
+      let body = ''
+      for (let j = 0; j < token.rows.length; j++) {
+        const row = token.rows[j]
+
+        cell = ''
+        for (let k = 0; k < row.length; k++) {
+          cell += this.tablecell(row[k])
+        }
+
+        body += this.tablerow({ text: cell })
+      }
+      if (body) body = `<tbody>${body}</tbody>`
+
+      return `<div class="v-table v-table--density-comfortable">
+  <div class="v-table__wrapper">
+    <table>
+      <thead>
+        ${header}
+      </thead>
+      ${body}
+    </table>
+  </div>
+</div>`
+    },
+    list (token: Tokens.List) {
+      const ordered = token.ordered
+      const start = token.start
+
+      let body = ''
+      for (let j = 0; j < token.items.length; j++) {
+        const item = token.items[j]
+        body += this.listitem(item)
+      }
+
+      const type = ordered ? 'ol' : 'ul'
+      const startAttr = (ordered && start !== 1) ? (' start="' + start + '"') : ''
+      return '<' + type + startAttr + ' class="ml-6 mb-6">\n' + body + '</' + type + '>\n'
+    }
+  }
+})
+
+const parse = (markdown: string) => marked.parse(markdown) as string
 
 export const renderMarkdown = (markdown: string) => sanitize(parse(markdown))
+// export const renderMarkdown = (markdown: string) => parse(markdown)
