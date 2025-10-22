33e6320379af1b1e5299381362b3608d2c113972
Alban Mouton <alban.mouton@gmail.com>
2025-10-22 17:41:07 +0200

    fix: prevent weird layout bug when hiding app bar

--- portal/app/layouts/default.vue
+++ portal/app/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <LayoutAppBar />
-  <v-main style="position: relative">
+  <v-main :style="`position: relative; padding-top: ${headerPadding}px;`">
     <v-container class="container">
       <slot />
     </v-container>
@@ -8,3 +8,11 @@
   <LayoutFooter />
   <LayoutScrollToTop />
 </template>
+
+<script setup lang="ts">
+const { portalConfig } = usePortalStore()
+// prevent a weird bug with the css var --v-layout-top that changes when we scroll while it shouldn't
+const headerPadding = computed(() => {
+  return 64 + (portalConfig.value.header.show ? 128 : 0)
+})
+</script>
--- portal/app/layouts/home.vue
+++ portal/app/layouts/home.vue
@@ -1,6 +1,6 @@
 <template>
   <LayoutAppBar :home="true" />
-  <v-main style="position: relative">
+  <v-main :style="`position: relative; padding-top: ${headerPadding}px;`">
     <v-container class="container">
       <slot />
     </v-container>
@@ -8,3 +8,12 @@
   <LayoutFooter :home="true" />
   <LayoutScrollToTop />
 </template>
+
+<script setup lang="ts">
+const { portalConfig } = usePortalStore()
+// prevent a weird bug with the css var --v-layout-top that changes when we scroll while it shouldn't
+const headerPadding = computed(() => {
+  const showHeader = portalConfig.value.headerHomeActive ? portalConfig.value.headerHome?.show : portalConfig.value.header.show
+  return 64 + (showHeader ? 128 : 0)
+})
+</script>
