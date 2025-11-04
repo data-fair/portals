0f0f5ba213248abb340d3b3eb5a860bdaddb30c2
BatLeDev <baptiste.guerny@koumoul.com>
2025-11-04 17:51:54 +0100

    feat: add redirection handler for explore paths

---
+++ portal/server/middleware/redirections.ts
@@ -0,0 +1,13 @@
+export default defineEventHandler((event) => {
+  const url = getRequestURL(event)
+  const path = url.pathname
+
+  if (path === '/explore/' || path === '/explore') {
+    return sendRedirect(event, '/datasets', 301)
+  }
+
+  if (path.startsWith('/explore/dataset/')) {
+    const id = path.replace('/explore/dataset/', '').split('/')[0]
+    return sendRedirect(event, `/datasets/${id}`, 301)
+  }
+})
