1a92019c51e18f97968e6ecc58dd202a6789ddec
Alban Mouton <alban.mouton@gmail.com>
2025-11-25 16:20:33 +0100

    chore: missing use of useAnalyticsInfo composable in plugin

--- portal/app/plugins/05-analytics.client.ts
+++ portal/app/plugins/05-analytics.client.ts
@@ -10,33 +10,30 @@ export default defineNuxtPlugin(async () => {
     console.log('ignore analytics configuration in draft mode')
     return
   }
-  const portalConfig = nuxtApp.$portal.config
-  const cookiePortalTrack = useCookie<string>('df_portal_track')
-  if (cookiePortalTrack.value === 'yes' || ((cookiePortalTrack.value + '') === '1')) {
-    // console.log('tracking was enabled by df_portal_track cookie')
-  } else {
-    // console.log('tracking was not enabled by df_portal_track cookie')
+  const portal = nuxtApp.$portal
+  const { requiresConsent, trackerType, cookieTrack } = useAnalyticsInfo(portal)
+  if (requiresConsent && cookieTrack.value !== 'yes') {
+    console.log('analytics tracking requires consent that was not yet granted')
     return
   }
 
-  const trackerType = portalConfig.analytics?.tracker.type
-  if (portalConfig.analytics && trackerType && trackerType !== 'none') {
+  if (portal.config.analytics && trackerType !== 'none') {
     const plugins: AnalyticsPlugin[] = []
-    if (portalConfig.analytics.tracker.type === 'google-analytics-v4') {
+    if (portal.config.analytics.tracker.type === 'google-analytics-v4') {
       // https://github.com/DavidWells/analytics/tree/master/packages/analytics-plugin-google-analytics
       // @ts-expect-error plugin not typed
       const ga = (await import('@analytics/google-analytics')).default
-      plugins.push(ga({ ...portalConfig.analytics.tracker.params }))
+      plugins.push(ga({ ...portal.config.analytics.tracker.params }))
     }
 
-    if (portalConfig.analytics.tracker.type === 'matomo') {
+    if (portal.config.analytics.tracker.type === 'matomo') {
       const matomo = (await import('../utils/analytics/matomo')).default
-      plugins.push(matomo({ ...portalConfig.analytics.tracker.params }))
+      plugins.push(matomo({ ...portal.config.analytics.tracker.params }))
     }
 
-    if (portalConfig.analytics.tracker.type === 'piano') {
+    if (portal.config.analytics.tracker.type === 'piano') {
       const piano = (await import('../utils/analytics/piano')).default
-      plugins.push(piano({ ...portalConfig.analytics.tracker.params }))
+      plugins.push(piano({ ...portal.config.analytics.tracker.params }))
     }
 
     const a = await import('analytics')
