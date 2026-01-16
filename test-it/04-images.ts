af7e0209ff43d8d73f52fcfbb5194361bca6b8f7
BatLeDev <baptiste.guerny@koumoul.com>
2026-01-16 16:48:16 +0100

    fix: support .ico images

--- test-it/04-images.ts
+++ test-it/04-images.ts
@@ -28,7 +28,7 @@ describe('images management', () => {
     assert.equal(image.width, 200)
     assert.equal(image.height, 200)
     assert.equal(image.name, 'logo.png')
-    assert.equal(image.mimeType, 'image/png')
+    assert.equal(image.mimeType, 'image/webp')
   })
 
   it('should load a SVG and not resize it', async () => {
@@ -42,5 +42,31 @@ describe('images management', () => {
     assert.equal(image.height, 0)
     assert.equal(image.name, 'logo.svg')
     assert.equal(image.mimeType, 'image/svg+xml')
+  })
+
+  it('should load an ICO (logo.ico) and convert it to WebP', async () => {
+    const page = (await user1.post('/api/pages', { type: 'home', config: { title: 'My page', elements: [] } })).data
+
+    const form = new FormData()
+    form.append('body', JSON.stringify({ resource: { type: 'page', _id: page._id } }))
+    form.append('image', createReadStream('test-it/resources/logo.ico'))
+    const image = await user1.post('/api/images', form).then(r => r.data)
+    assert(image.width > 0, 'width should be positive')
+    assert(image.height > 0, 'height should be positive')
+    assert.equal(image.name, 'logo.ico')
+    assert.equal(image.mimeType, 'image/webp')
+  })
+
+  it('should load another ICO (logo2.ico) and convert it to WebP', async () => {
+    const page = (await user1.post('/api/pages', { type: 'home', config: { title: 'My page', elements: [] } })).data
+
+    const form = new FormData()
+    form.append('body', JSON.stringify({ resource: { type: 'page', _id: page._id } }))
+    form.append('image', createReadStream('test-it/resources/logo2.ico'))
+    const image = await user1.post('/api/images', form).then(r => r.data)
+    assert(image.width > 0, 'width should be positive')
+    assert(image.height > 0, 'height should be positive')
+    assert.equal(image.name, 'logo2.ico')
+    assert.equal(image.mimeType, 'image/webp')
   })
 })
