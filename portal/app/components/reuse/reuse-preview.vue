d771ebada24c1b72f5382de899753dd36614db1c
BatLeDev <baptiste.guerny@koumoul.com>
2026-01-28 09:21:01 +0100

    fix: reuse image in preview

--- portal/app/components/reuse/reuse-preview.vue
+++ portal/app/components/reuse/reuse-preview.vue
@@ -110,6 +110,7 @@ const { portal, portalConfig, preview } = usePortalStore()
 
 const getReuseImageSrc = (imageRef: ImageRef, mobile: boolean) => {
   let id = imageRef._id
+  if (preview) return `/portals-manager/api/images/${id}/data`
   if (mobile && imageRef.mobileAlt) id += '-mobile'
   return `/portal/api/reuses/${slug}/images/${id}`
 }
