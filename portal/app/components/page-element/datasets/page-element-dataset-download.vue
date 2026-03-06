37925c3482cd3a7fe0cdbab8322e2d891949e889
BatLeDev <baptiste.guerny@koumoul.com>
2026-03-06 11:44:02 +0100

    feat: add dataset-download iframe

---
+++ portal/app/components/page-element/datasets/page-element-dataset-download.vue
@@ -0,0 +1,26 @@
+<template>
+  <d-frame-wrapper
+    v-if="element.dataset"
+    :class="element.mb !== 0 && `mb-${element.mb ?? 4}`"
+    :iframe-title="`${t('datasetDownload')} - ${element.dataset.title}`"
+    :src="`/data-fair/embed/dataset/${element.dataset.id}/download?header=true`"
+    scrolling="no"
+    resize="yes"
+    sync-params
+  />
+</template>
+
+<script setup lang="ts">
+import type { DatasetDownloadElement } from '#api/types/page-elements/index.ts'
+
+const { element } = defineProps<{ element: DatasetDownloadElement }>()
+const { t } = useI18n()
+
+</script>
+
+<i18n lang="yaml">
+  en:
+    datasetDownload: Download
+  fr:
+    datasetDownload: Téléchargement
+</i18n>
