c2380704a25e3611cb1ec0c3b995be1fda6e83fb
Alban Mouton <alban.mouton@gmail.com>
2026-04-07 15:42:42 +0200

    feat: add an alternative page edition page with single vjsf form

---
+++ api/types/page-config-simple/schema.js
@@ -0,0 +1,47 @@
+import pageElementsSchema from '../page-elements/schema.js'
+import pageConfigSchema from '../page-config/schema.js'
+
+// Re-use the element definition from page-elements but remove the page-preview-element summary slot
+const element = {
+  ...pageElementsSchema.$defs.element,
+  layout: {
+    getDefaultData: pageElementsSchema.$defs.element.layout.getDefaultData
+    // no switch with page-preview-element summary slot
+  }
+}
+
+export default {
+  $id: 'https://github.com/data-fair/portals/page-config-simple',
+  'x-exports': ['vjsf'],
+  'x-jstt': { additionalProperties: false },
+  'x-vjsf': {
+    pluginsImports: ['@koumoul/vjsf-markdown'],
+    xI18n: true,
+    webmcp: true
+  },
+  'x-vjsf-locales': ['en', 'fr'],
+  title: 'PageConfigSimple',
+  type: 'object',
+  unevaluatedProperties: false,
+  layout: pageConfigSchema.layout,
+  required: pageConfigSchema.required,
+  properties: {
+    ...pageConfigSchema.properties,
+    elements: {
+      type: 'array',
+      layout: {
+        title: '',
+        clipboardKey: 'elements',
+        listEditMode: 'dialog',
+        itemCopy: "{...item, uuid: crypto.randomUUID().split('-')[0]}"
+      },
+      items: {
+        $ref: '#/$defs/element'
+      }
+    }
+  },
+  $defs: {
+    ...pageConfigSchema.$defs,
+    element
+  }
+}
