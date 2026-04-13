fef3483b2b240871fc8fbe04049356588ac2d674
BatLeDev <baptiste.guerny@koumoul.com>
2026-04-13 16:12:46 +0200

    fix(portal): add missing providePageImageSrc call in reuse page

--- portal/app/pages/reuses/[slug].vue
+++ portal/app/pages/reuses/[slug].vue
@@ -33,6 +33,7 @@ const slug = route.params.slug as string
 const { t } = useI18n()
 const { portalConfig, preview } = usePortalStore()
 const { setBreadcrumbs } = useNavigationStore()
+providePageImageSrc('reuses', slug)
 
 type BreadcrumbItem = NonNullable<VBreadcrumbs['$props']['items']>[number]
 
